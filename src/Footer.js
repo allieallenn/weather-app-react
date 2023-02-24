38f649c643418ef7ab5b2ad399572b3c2d3aa11c
allieallenn <117948484+allieallenn@users.noreply.github.com>
2023-02-24 14:39:39 -0600

    added footer

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      This project was coded by{" "}
+      <a href="https://github.com/allieallenn">Alicia Allen</a> and is{" "}
+      <a href="https://github.com/allieallenn/weather-app-react">
+        open-sourced on GitHub
+      </a>
+    </footer>
+  );
+}
